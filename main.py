fa00ee761e9bcda3c3eb9aecedad9d8e45e380a2
Akshath Thoguparthy <thoguparthy@gmail.com>
2026-05-26 20:12:25 +0530

    Adding small program to learn about basic get api call

--- main.py
+++ main.py
@@ -1,25 +1,73 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: bool | None = None
-
-
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def home():
+    return {"message":"Hello World"}
+
+@app.get("/add")
+def add(a: int, b:int):
+    return {"sum":a+b}
+
+@app.get("/user/{name}")
+def name(name:str):
+    return {"name":f"Hello {name}"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
 
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+
+# app = FastAPI()
+
+
+# class Item(BaseModel):
+#     name: str
+#     price: float
+#     is_offer: bool | None = None
+
+
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
+
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: str | None = None):
+#     return {"item_id": item_id, "q": q}
+
+
+# @app.put("/items/{item_id}")
+# def update_item(item_id: int, item: Item):
+#     return {"item_name": item.name, "item_id": item_id}
